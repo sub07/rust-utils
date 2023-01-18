0d211df14d03372252b8e1920104370d1a8c10b5
sub07 <pardomarius@gmail.com>
2023-01-18 14:19:07 +0100

    enum value done

--- examples/enum_iter.rs
+++ examples/enum_iter.rs
@@ -2,13 +2,13 @@ use rust_utils::{EnumIter, EnumValue};
 
 #[derive(EnumIter, EnumValue, Debug)]
 pub enum TestEnum2 {
-    #[value(offset: f32 = 1.0)]
+    #[value(offset: f32 = 1.8)]
     Var1,
-    #[value(offset: f32 = 2.0)]
+    #[value(offset: f32 = 2.56)]
     Var2,
-    #[value(offset: f32 = 3.0)]
+    #[value(offset: f32 = 3.2)]
     Var3,
-    #[value(offset: f32 = 4.0)]
+    #[value(offset: f32 = 4.4)]
     Var4,
 }
 
@@ -18,4 +18,7 @@ fn main() {
     println!("{:?}", TestEnum2::VARIANTS);
     println!("{:?}", TestEnum2::variants());
     println!("{:?}", TestEnum2::iter());
+    for val in TestEnum2::iter() {
+        println!("{:?} {}", val, val.offset());
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,175 @@
+extern crate core;
+
 use proc_macro::TokenStream;
 use std::collections::HashMap;
 use std::env::var;
 
 use proc_macro2::Literal;
 use quote::quote;
-use syn::{Data, DeriveInput, Ident, parenthesized, parse_macro_input, Token, token, Type, TypePath};
+use syn::{Data, DataEnum, DeriveInput, Ident, parenthesized, parse_macro_input, Token, token, Type, TypePath};
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::token::Token;
 
+fn get_enum_data(input: &DeriveInput) -> &DataEnum {
+    if let Data::Enum(data_enum) = &input.data {
+        data_enum
+    } else {
+        panic!("Must be applied on enum")
+    }
+}
+
 #[proc_macro_derive(EnumIter)]
 pub fn enum_iter_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    if let Data::Enum(data_enum) = &input.data {
-        let mut variants = quote! {};
-        let enum_name = &input.ident;
-        for variant in &data_enum.variants {
-            let variant_ident = &variant.ident;
-            variants.extend(quote! { #enum_name::#variant_ident, })
-        }
-        let array = quote! { [#variants] };
-        let nb_variants = data_enum.variants.len();
-        let impl_enum = quote! {
-            impl #enum_name {
-                pub const SIZE: usize = #enum_name::size();
-                pub const VARIANTS: [#enum_name; #nb_variants] = #enum_name::variants();
-                pub const fn size() -> usize { #nb_variants }
-                pub const fn variants() -> [#enum_name; #nb_variants] {
-                    #array
-                }
-                pub fn iter() -> std::slice::Iter<'static, #enum_name> {
-                    #enum_name::VARIANTS.iter()
-                }
-            }
-        };
-
-        impl_enum.into()
-    } else {
-        panic!("EnumIter must be applied on enum only")
+    let enum_data = get_enum_data(&input);
+    let mut variants = quote! {};
+    let enum_name = &input.ident;
+    for variant in &enum_data.variants {
+        let variant_ident = &variant.ident;
+        variants.extend(quote! { #enum_name::#variant_ident, })
     }
+    let array = quote! { [#variants] };
+    let nb_variants = enum_data.variants.len();
+    let impl_enum = quote! {
+        impl #enum_name {
+            pub const SIZE: usize = #enum_name::size();
+            pub const VARIANTS: [#enum_name; #nb_variants] = #enum_name::variants();
+            pub const fn size() -> usize { #nb_variants }
+            pub const fn variants() -> [#enum_name; #nb_variants] {
+                #array
+            }
+            pub fn iter() -> std::slice::Iter<'static, #enum_name> {
+                #enum_name::VARIANTS.iter()
+            }
+        }
+    };
+
+    impl_enum.into()
 }
 
 #[proc_macro_derive(EnumValue, attributes(value))]
 pub fn enum_variant_associate_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    if let Data::Enum(data_enum) = input.data {
-        let mut res = quote!();
-        let enum_name = &input.ident;
-        let mut values = HashMap::new();
-        let mut type_name = None;
-        let mut ident= None;
-        for variant in data_enum.variants {
-            let attr = variant.attrs.iter()
-                .map(|a| &a.tokens).next()
-                .expect(&format!("No value on variant {}", variant.ident))
-                .clone();
-            let v = syn::parse2::<ValueContent>(attr).expect("Could not parse").value;
-            type_name = Some(v.type_name);
-            ident = Some(v.ident);
-            values.insert(variant.ident, v.value);
+    let enum_data = get_enum_data(&input);
+    let enum_name = &input.ident;
+    let attributes_content =
+        enum_data
+            .variants.iter()
+            .map(|v|
+                FieldAttributes {
+                    variant_ident: v.ident.clone(),
+                    attribute_content: syn::parse2::<AttributeContent>(
+                        v.attrs.iter().next()
+                            .expect(&format!("Illegal variant without value attribute : {}", v.ident))
+                            .tokens.clone()
+                    ).expect("Invalid syntax in #[value(...)] : "),
+                }
+            ).collect::<Vec<_>>();
+
+    let nb_field = get_nb_field(&attributes_content);
+
+    for field_index in 0..nb_field {
+        let values = attributes_content.iter()
+            .map(|f|
+                f.attribute_content
+                    .values.iter()
+                    .nth(field_index).unwrap()
+            )
+            .collect::<Vec<_>>();
+        let idents = values.iter().map(|v| v.ident.to_string()).collect::<Vec<_>>();
+        let types = values.iter()
+            .map(|v|
+                v.type_name.path
+                    .get_ident().expect("Only single segments paths type are supported")
+                    .to_string()
+            )
+            .collect::<Vec<_>>();
+
+        if !all_equals(&idents) {
+            panic!("Field n°{} names are not all equals : {idents:#?}", field_index + 1);
         }
 
-        let mut match_content = quote!();
+        if !all_equals(&types) {
+            panic!("Field n°{} types are not all equals : {types:#?}", field_index + 1);
+        }
+    }
 
-        for (variant, value) in values {
-            match_content.extend(quote! {
-                #enum_name::#variant => #value,
+    let mut functions = quote!();
+    for field_index in 0..nb_field {
+        let (field_name, type_name) = {
+            let value = &attributes_content[field_index].attribute_content.values[field_index];
+            (value.ident.clone(), value.type_name.clone())
+        };
+
+        let literals =
+            attributes_content.iter().map(|attr| (attr.variant_ident.clone(), attr.attribute_content.values[field_index].value.clone())).collect::<Vec<_>>();
+
+        let mut match_statement = quote!();
+
+        for (ident, literal) in literals {
+            match_statement.extend(quote! {
+                #enum_name::#ident => #literal,
             });
         }
 
-        res.extend(quote! {
-            impl #enum_name {
-                pub fn #ident(&self) -> #type_name {
-                    match self {
-                        #match_content
-                    }
+        functions.extend(quote! {
+            pub fn #field_name(&self) -> #type_name {
+                match self {
+                    #match_statement
                 }
             }
-        });
-        res.into()
-    } else {
-        panic!("EnumValue must be applied on enum only")
+        })
     }
+
+    let impl_block = quote! {
+        impl #enum_name {
+            #functions
+        }
+    };
+
+    impl_block.into()
+}
+
+fn all_equals<T: Eq>(idents: &[T]) -> bool {
+    let first = idents.iter().next();
+    idents.iter().fold(first, |acc, ident| {
+        acc.and_then(|acc| if acc == ident { Some(acc) } else { None })
+    }).is_some()
+}
+
+fn get_nb_field(attributes_content: &[FieldAttributes]) -> usize {
+    let values_length = attributes_content.iter().map(|attr| attr.attribute_content.values.len()).collect::<Vec<_>>();
+
+    if values_length.is_empty() {
+        panic!("No field in value");
+    }
+
+    if values_length.iter().max() != values_length.iter().min() { // All values are equals
+        panic!("Must have the same number of fields in all values");
+    }
+
+    let nb_field = *values_length.iter().next().unwrap();
+    nb_field
+}
+
+struct FieldAttributes {
+    variant_ident: Ident,
+    attribute_content: AttributeContent,
 }
 
 #[derive(Debug)]
-struct ValueContent {
+struct AttributeContent {
     paren_token: token::Paren,
-    value: Value,
+    values: Punctuated<Value, Token![,]>,
 }
 
-impl Parse for ValueContent {
+impl Parse for AttributeContent {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let content;
-        Ok(ValueContent {
+        Ok(AttributeContent {
             paren_token: parenthesized!(content in input),
-            value: content.parse()?,
+            values: content.parse_terminated(Value::parse)?,
         })
     }
 }
