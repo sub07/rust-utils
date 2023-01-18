b84fcc35e24645dc759cbddbdb4a422ed4deee49
sub07 <pardomarius@gmail.com>
2023-01-18 01:05:46 +0100

    parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Literal};
+use std::collections::HashMap;
+use std::env::var;
+
+use proc_macro2::Literal;
 use quote::quote;
 use syn::{Data, DeriveInput, Ident, parenthesized, parse_macro_input, Token, token, Type, TypePath};
 use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
 use syn::token::Token;
 
 #[proc_macro_derive(EnumIter)]
@@ -40,19 +44,47 @@ pub fn enum_iter_derive(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(EnumValue, attributes(value))]
 pub fn enum_variant_associate_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let res = quote!();
-    if let Data::Enum(data_enum) = &input.data {
-        for variant in &data_enum.variants {
+    if let Data::Enum(data_enum) = input.data {
+        let mut res = quote!();
+        let enum_name = &input.ident;
+        let mut values = HashMap::new();
+        let mut type_name = None;
+        let mut ident= None;
+        for variant in data_enum.variants {
             let attr = variant.attrs.iter()
                 .map(|a| &a.tokens).next()
                 .expect(&format!("No value on variant {}", variant.ident))
                 .clone();
-            let value = syn::parse2::<ValueContent>(attr).expect("Could not parse");
+            let v = syn::parse2::<ValueContent>(attr).expect("Could not parse").value;
+            type_name = Some(v.type_name);
+            ident = Some(v.ident);
+            values.insert(variant.ident, v.value);
         }
+
+        let mut match_content = quote!();
+
+        for (variant, value) in values {
+            match_content.extend(quote! {
+                #enum_name::#variant => #value,
+            });
+        }
+
+        res.extend(quote! {
+            impl #enum_name {
+                pub fn #ident(&self) -> #type_name {
+                    match self {
+                        #match_content
+                    }
+                }
+            }
+        });
+        res.into()
+    } else {
+        panic!("EnumValue must be applied on enum only")
     }
-    res.into()
 }
 
+#[derive(Debug)]
 struct ValueContent {
     paren_token: token::Paren,
     value: Value,
@@ -63,11 +95,12 @@ impl Parse for ValueContent {
         let content;
         Ok(ValueContent {
             paren_token: parenthesized!(content in input),
-            value: content.parse().expect("value parse err"),
+            value: content.parse()?,
         })
     }
 }
 
+#[derive(Debug)]
 struct Value {
     ident: Ident,
     colon: Token!(:),
@@ -78,7 +111,7 @@ struct Value {
 
 impl Parse for Value {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        Ok(Value{
+        Ok(Value {
             ident: input.parse()?,
             colon: input.parse()?,
             type_name: input.parse()?,
