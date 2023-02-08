823508542eadcb124ee9730bfeda160f16054af9
sub07 <pardomarius@gmail.com>
2023-02-09 00:18:11 +0100

    add Number trait

---
+++ rust-utils/src/number.rs
@@ -0,0 +1,11 @@
+trait Number: Copy + Clone + PartialOrd + PartialEq {}
+
+macro_rules! impl_primitive {
+    ($($ty:path)*) => {
+        $(
+            impl Number for $ty {}
+        )*
+    };
+}
+
+impl_primitive!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize f32 f64);
