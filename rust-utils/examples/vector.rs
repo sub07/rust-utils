b6782e3f3245bff8917aacb6d3ccc4ed800b32b5
sub07 <pardomarius@gmail.com>
2023-02-09 18:53:02 +0100

    vector operation

--- rust-utils/examples/vector.rs
+++ rust-utils/examples/vector.rs
@@ -1,13 +1,11 @@
-#![feature(box_syntax)]
-
 use rust_utils::vector::Vector;
 
 fn main() {
-    let pos1 = Vector::from([5; 2]);
-    let pos2 = Vector::from([6; 2]);
-    let sum = pos1 + pos2;
+    let pos1 = Vector::from([5; 3]);
+    let pos2 = Vector::from([6; 3]);
+    let sum = &pos1 + &pos2 * &pos1;
 
     dbg!(sum);
-    // dbg!(pos1);
-    // dbg!(pos2);
+    dbg!(pos1);
+    dbg!(pos2);
 }
--- rust-utils/src/vector.rs
+++ rust-utils/src/vector.rs
@@ -3,26 +3,47 @@ use crate::number::Number;
 #[derive(Debug)]
 pub struct Vector<T: Number, const SIZE: usize>([T; SIZE]);
 
-impl<T: Number, const SIZE: usize> Vector<T, SIZE> {
+impl <T: Number, const SIZE: usize> Vector<T, SIZE> {
     pub fn from(values: [T; SIZE]) -> Vector<T, SIZE> {
         Vector(values)
     }
+
+    pub fn as_slice(&self) -> &[T; SIZE] {
+        &self.0
+    }
 }
 
-macro_rules! impl_vec_op {
-    (+, )
-    ($trait_name:ident, $fn_name:ident, $impl_ty:ty, $rhs_ty:ty, $output_ty:ty, $($fn_body:tt)+) => {
+macro_rules! impl_vec_ops_binary {
+    () => ();
+    ($op:tt $($other:tt)*) => {
+      impl_vec_op_binary!($op);
+      impl_vec_ops_binary!($($other)*);
+    };
+}
+
+macro_rules! impl_vec_op_binary {
+    (+) => (impl_vec_op_binary!(+, Add, add););
+    (-) => (impl_vec_op_binary!(-, Sub, sub););
+    (*) => (impl_vec_op_binary!(*, Mul, mul););
+    (/) => (impl_vec_op_binary!(/, Div, div););
+    ($op:tt, $trait_name:ident, $fn_name:ident) => {
+        impl_vec_op_binary!($op, $trait_name, $fn_name, Vector<T, SIZE>, Vector<T, SIZE>);
+        impl_vec_op_binary!($op, $trait_name, $fn_name, &Vector<T, SIZE>, Vector<T, SIZE>);
+        impl_vec_op_binary!($op, $trait_name, $fn_name, Vector<T, SIZE>, &Vector<T, SIZE>);
+        impl_vec_op_binary!($op, $trait_name, $fn_name, &Vector<T, SIZE>, &Vector<T, SIZE>);
+    };
+    ($op:tt, $trait_name:ident, $fn_name:ident, $ty1:ty, $ty2:ty) => {
+        impl_vec_op_binary!($trait_name, $fn_name, |l: $ty1, r: $ty2| -> Vector<T, SIZE> { Vector(l.0.zip(r.0).map(|(l, r)| l $op r)) });
+    };
+    ($trait_name:ident, $fn_name:ident, |$left:ident:$impl_ty:ty, $right:ident:$rhs_ty:ty| -> $output_ty:ty $fn_body:block) => {
         impl <T: Number, const SIZE: usize> std::ops::$trait_name<$rhs_ty> for $impl_ty {
             type Output = $output_ty;
 
-            fn $fn_name(self, rhs: Vector<T, SIZE>) -> Self::Output {
-                $($fn_body)+
+            fn $fn_name(self, rhs: $rhs_ty) -> Self::Output {
+                |$left:$impl_ty, $right:$rhs_ty| -> $output_ty { $fn_body }(self, rhs)
             }
         }
     };
 }
 
-impl_vec_op!(Add, add, Vector<T, SIZE>, Vector<T, SIZE>, Vector<T, SIZE>, Vector(self.0.zip(rhs.0).map(|(left, right)| left + right)));
-// impl_vec_op!(Sub, sub, |(l, r)| l - r);
-// impl_vec_op!(Mul, mul, |(l, r)| l * r);
-// impl_vec_op!(Div, div, |(l, r)| l / r);
+impl_vec_ops_binary!(+ - * /);
