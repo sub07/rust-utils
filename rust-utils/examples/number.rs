13b3538c3d90978f3507d840fc6edff7f994001d
sub07 <pardomarius@gmail.com>
2023-04-11 20:45:20 +0200

    remove FromPrimitive trait from examples

--- rust-utils/examples/number.rs
+++ rust-utils/examples/number.rs
@@ -10,11 +10,4 @@ fn f1(i: i32) {
 
 fn main() {
     f(6);
-    // f(true);
-    let i = 56u32;
-    f1(i.into_p());
-    let i = 56i8;
-    f1(i.into_p());
-    let i = 56f32;
-    f1(i.into_p());
 }
