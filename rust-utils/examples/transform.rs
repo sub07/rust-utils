18fe328084d4c716a9c3cbcf27697f222e0d304e
sub07 <pardomarius@gmail.com>
2024-02-12 08:10:21 +0000

    test transform

--- rust-utils/examples/transform.rs
+++ rust-utils/examples/transform.rs
@@ -1,6 +1,4 @@
-use std::convert::Infallible;
-
-use rust_utils::{into, try_into};
+use std::{convert::Infallible, fmt::{Debug, Display, Write}, marker::PhantomData};
 
 #[derive(Debug, Copy, Clone)]
 struct A;
@@ -11,31 +9,96 @@ struct B;
 #[derive(Debug, Copy, Clone)]
 struct C;
 
-impl From<A> for B {
-    fn from(_value: A) -> Self {
+pub trait ConvertInto<O> {
+    fn convert_into(self) -> O;
+}
+
+pub trait TryConvertInto<O> {
+    type Error;
+
+    fn try_convert_into(self) -> Result<O, Self::Error>;
+}
+
+impl ConvertInto<B> for A {
+    fn convert_into(self) -> B {
         B
     }
 }
 
-impl From<B> for C {
-    fn from(_value: B) -> Self {
+impl ConvertInto<C> for B {
+    fn convert_into(self) -> C {
         C
     }
 }
 
-impl From<i32> for B {
-    fn from(_value: i32) -> Self {
+impl ConvertInto<B> for i32 {
+    fn convert_into(self) -> B {
         B
     }
 }
 
+// impl<T: ConvertInto<T>> TryConvertInto<T> for T {
+//     type Error = Infallible;
+
+//     fn try_convert_into(self) -> Result<T, Self::Error> {
+//         Ok(self.convert_into())
+//     }
+// }
+
+// #[macro_export]
+// macro_rules! convert {
+//     ($input:tt -> $($chain:ident)->*) => {
+//         convert!(@internal $input < [$($chain) *])
+//     };
+//     (@internal $input:tt < [$first:tt $($rest:tt)*] $($reversed:tt)*) => {
+//         convert!(@internal $input < [$($rest)*] $first $($reversed)*)
+//     };
+//     (@internal $input:tt < [] $($reversed:tt)*) => {
+//         convert!(@internal $input | $($reversed),*)
+//     };
+//     (@internal $input:tt | $output_ty:ident, $($rest_output:ident),+) => {
+//         ConvertInto::<$output_ty>::convert_into(convert!(@internal $input | $($rest_output),+))
+//     };
+//     (@internal $input:tt | $output_ty:ident) => {
+//         ConvertInto::<$output_ty>::convert_into($input)
+//     };
+// }
+
+// pub fn try_convert_into_helper<F, I, E>(from: Result<F, E>) -> Result<I, E>
+// where
+//     F: TryConvertInto<I, Error = E>,
+// {
+//     TryConvertInto::<I>::try_convert_into(from?)
+// }
+
+// #[macro_export]
+// macro_rules! try_into {
+//     ($input:tt -> $($chain:ident)->*) => {
+//         try_into!(@internal $input < [$($chain) *])
+//     };
+//     (@internal $input:tt < [$first:tt $($rest:tt)*] $($reversed:tt)*) => {
+//         try_into!(@internal $input < [$($rest)*] $first $($reversed)*)
+//     };
+//     (@internal $input:tt < [] $($reversed:tt)*) => {
+//         try_into!(@internal $input | $($reversed),*)
+//     };
+//     (@internal $input:tt | $output_ty:ident, $($rest_output:ident),+) => {
+//         $crate::transform::try_into_helper::<_, $output_ty, _>(try_into!(@internal $input | $($rest_output),+))
+//     };
+//     (@internal $input:tt | $output_ty:ident) => {
+//         $crate::transform::try_into_helper::<_, $output_ty, _>(Ok($input))
+//     };
+// }
+
 fn main() {
-    let a = A;
+    // let a = A;
 
-    let _c = into!(a -> B -> C);
-    let _c = into!(2i32 -> B -> C);
+    // let _c = convert!(a -> B -> C);
+    // let _c = convert!(2i32 -> B -> C);
 
-    let _c = try_into!(A -> B -> C);
-    let _c = try_into!(a -> B -> C);
-    let _c = try_into!(2i32 -> B -> C);
+    // let _c = try_into!(A -> B -> C);
+    // let _c = try_into!(a -> B -> C);
+    // let _c = try_into!(2i32 -> B -> C);
+
+    // let b = try_convert_into_helper::<_, B, _>(Ok(A));
 }
