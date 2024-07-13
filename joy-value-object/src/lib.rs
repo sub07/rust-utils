f86bcbb27000c0c4280f948b06b0bd8eea9f1c22
Marius Pardo <pardomarius@gmail.com>
2024-07-13 16:39:59 +0200

    value object: enable additional derive

--- joy-value-object/src/lib.rs
+++ joy-value-object/src/lib.rs
@@ -9,8 +9,8 @@ pub enum Bound {
 
 #[macro_export]
 macro_rules! mk_vo {
-    ($vis:vis $name:ident: $ty:ty, default: $default:expr, min: $min:expr, max: $max:expr $(,)?) => {
-        #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+    ($vis:vis $name:ident: $ty:ty, default: $default:expr, min: $min:expr, max: $max:expr $(, additionnal_derive: $additionnal_derive:tt)? $(,)?) => {
+        #[derive(Debug, Clone, Copy, PartialEq, PartialOrd $(, $additionnal_derive)?)]
         $vis struct $name($ty);
 
         impl $name {
@@ -175,6 +175,7 @@ mod test {
         default: 0,
         min: -12,
         max: 34,
+        additionnal_derive: Hash,
     }
 
     mk_vo_consts! {
