8fdf72a30b9606b10063834347ffffb8d85f16cb
Marius Pardo <pardomarius@gmail.com>
2025-01-08 15:48:18 +0100

    add result error logging

--- joy-error/src/lib.rs
+++ joy-error/src/lib.rs
@@ -1,3 +1,7 @@
+use std::fmt::Display;
+
+use tracing::error;
+
 pub trait OptionToResultExt<T, E> {
     fn unwrap_or_fallible<F>(self, f: F) -> Result<T, E>
     where
@@ -13,5 +17,35 @@ impl<T, E> OptionToResultExt<T, E> for Option<T> {
             Some(value) => Ok(value),
             None => f(),
         }
+    }
+}
+
+pub trait ResultLogExt<T, E> {
+    #[must_use]
+    fn log_err(self) -> Self;
+
+    /// # Errors
+    ///
+    /// This function will return an error if self is an error.
+    fn log_and_map_err<F, O: FnOnce(E) -> F>(self, op: O) -> Result<T, F>;
+
+    #[must_use]
+    fn log_ok(self) -> Option<T>;
+}
+
+impl<T, E: Display> ResultLogExt<T, E> for Result<T, E> {
+    #[inline]
+    fn log_err(self) -> Self {
+        self.inspect_err(|e| error!("{e}"))
+    }
+
+    #[inline]
+    fn log_and_map_err<F, O: FnOnce(E) -> F>(self, op: O) -> Result<T, F> {
+        self.log_err().map_err(op)
+    }
+
+    #[inline]
+    fn log_ok(self) -> Option<T> {
+        self.log_err().ok()
     }
 }
