c24ce264d119a38c59d4c466cfaa6e1a998d00d3
Marius Pardo <marius.pardo@guarani.fr>
2025-05-20 14:06:29 +0200

    allow enumstr for enum with data

--- joy-macro/examples/enum_str.rs
+++ joy-macro/examples/enum_str.rs
@@ -1,14 +1,31 @@
 use joy_macro::EnumStr;
 
 #[derive(EnumStr)]
-enum Test {
+enum Test1 {
     Variant1,
     Variant2,
     V,
 }
 
+#[derive(EnumStr)]
+enum Test2 {
+    Variant1(()),
+    Variant2 { _test: String },
+    V,
+}
+
 fn main() {
-    assert_eq!("V", Test::V.as_str());
-    assert_eq!("Variant1", Test::Variant1.as_str());
-    assert_eq!("Variant2", Test::Variant2.as_str());
+    assert_eq!("V", Test1::V.as_str());
+    assert_eq!("Variant1", Test1::Variant1.as_str());
+    assert_eq!("Variant2", Test1::Variant2.as_str());
+
+    assert_eq!("Variant1", Test2::Variant1(()).as_str());
+    assert_eq!(
+        "Variant2",
+        Test2::Variant2 {
+            _test: "test".into()
+        }
+        .as_str()
+    );
+    assert_eq!("V", Test2::V.as_str());
 }
--- joy-macro/src/enum_str.rs
+++ joy-macro/src/enum_str.rs
@@ -1,22 +1,45 @@
+use itertools::Itertools;
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{DataEnum, Ident};
 
 pub fn derive(enum_ident: Ident, enum_data: DataEnum) -> TokenStream {
     let variants = enum_data.variants.iter().collect::<Vec<_>>();
-    let variants_str = variants
+    let variants_str = variants.iter().map(|v| v.ident.to_string()).collect_vec();
+    let variants_ident = variants.iter().map(|v| &v.ident).collect_vec();
+    let variants_destruct = variants
         .iter()
-        .map(|v| v.ident.to_string())
-        .collect::<Vec<_>>();
-    quote! {
+        .map(|variant| match &variant.fields {
+            syn::Fields::Named(_) => quote! { { .. } },
+            syn::Fields::Unnamed(fields_unnamed) => {
+                let fields = fields_unnamed
+                    .unnamed
+                    .iter()
+                    .map(|_| quote! {_})
+                    .collect_vec();
+                quote! {
+                    (#(#fields,)*)
+                }
+            }
+            syn::Fields::Unit => TokenStream::new(),
+        })
+        .collect_vec();
+    let const_token = if variants_destruct.iter().any(|t| !t.is_empty()) {
+        quote! {const}
+    } else {
+        TokenStream::new()
+    };
+    let t = quote! {
         impl #enum_ident {
-            pub const fn as_str(self) -> &'static str {
+            pub #const_token fn as_str(&self) -> &'static str {
                 match self {
                     #(
-                        #enum_ident::#variants => #variants_str
+                        #enum_ident::#variants_ident #variants_destruct => #variants_str
                     ),*
                 }
             }
         }
-    }
+    };
+    println!("{t}");
+    t
 }
