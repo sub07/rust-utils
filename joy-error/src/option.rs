a7103d4dacf67412b9e85678581b1387649a2897
Marius Pardo <marius.pardo@guarani.fr>
2025-12-26 11:45:39 +0100

    add and_then_zip mut variant

--- joy-error/src/option.rs
+++ joy-error/src/option.rs
@@ -6,6 +6,13 @@ pub impl<T> Option<T> {
     {
         self.and_then(|value| f(&value).map(|r| (value, r)))
     }
+
+    fn and_then_zip_mut<F, R>(self, f: F) -> Option<(T, R)>
+    where
+        F: FnOnce(&mut T) -> Option<R>,
+    {
+        self.and_then(|mut value| f(&mut value).map(|r| (value, r)))
+    }
 }
 
 #[cfg(test)]
@@ -13,7 +20,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn zip_with_test() {
+    fn and_then_zip_test() {
         let some_value = Some(5);
 
         let add_if_even = |x: &i32| {
@@ -29,5 +36,25 @@ mod test {
 
         let res = Some(4).and_then_zip(|value| add_if_even(value));
         assert_eq!(Some((4, 14)), res);
+    }
+
+    #[test]
+    fn and_then_zip_mut_test() {
+        let some_value = Some(5);
+
+        let add_if_even = |x: &mut i32| {
+            if *x % 2 == 0 {
+                *x += 10;
+                Some(*x)
+            } else {
+                None
+            }
+        };
+
+        let res = some_value.and_then_zip_mut(|value| add_if_even(value));
+        assert_eq!(None, res);
+
+        let res = Some(4).and_then_zip_mut(|value| add_if_even(value));
+        assert_eq!(Some((14, 14)), res);
     }
 }
